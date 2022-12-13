8e49f595b66f24c6d468c2e41c481699024402c0
Daria Timoshevskaya <vileonika@gmail.com>
2022-12-13 20:16:35 +0300

    step 4

--- bin/brain-games.js
+++ bin/brain-games.js
@@ -2,5 +2,5 @@
 
 import { askUserName } from '../src/cli.js';
 
-console.log("Welcome to the Brain Games!");
+console.log('Welcome to the Brain Games!');
 askUserName();
--- src/cli.js
+++ src/cli.js
@@ -1,6 +1,6 @@
 import readlineSync from 'readline-sync';
 
 export const askUserName = () => {
-    const userName = readlineSync.question('May I have your name?: ');
-    console.log(`Hello, ${userName}!`);
+  const userName = readlineSync.question('May I have your name?: ');
+  console.log(`Hello, ${userName}!`);
 };
