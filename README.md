b8c50e4169a34d9ea411fdca2e9ba44c74c83906
Daria Timoshevskaya <vileonika@gmail.com>
2022-12-16 22:25:16 +0300

    step 5

--- README.md
+++ README.md
@@ -2,3 +2,5 @@
 [![Actions Status](https://github.com/vikileon/frontend-project-44/workflows/hexlet-check/badge.svg)](https://github.com/vikileon/frontend-project-44/actions)
 
 <a href="https://codeclimate.com/github/vikileon/frontend-project-44/maintainability"><img src="https://api.codeclimate.com/v1/badges/becf06779a2146e1a977/maintainability" /></a>
+
+https://asciinema.org/a/UjYd63xXMHI73dcYP2Ipx6osI
---
+++ bin/brain-even.js
@@ -0,0 +1,42 @@
+#!/usr/bin/env node
+
+import readlineSync from 'readline-sync';
+
+console.log('Welcome to the Brain Games!');
+
+const isEven = () => {
+  const userName = readlineSync.question('May I have your name?: ');
+  console.log(`Hello, ${userName}!`);
+
+  console.log('Answer "yes" if the number is even, otherwise answer "no".');
+  let correctAnswers = 0;
+
+  for (let i = 0; i <= 2; i += 1) {
+    const randomNumber = Math.round(Math.random() * (100 - 1) + 1);
+    const yourAnswer = readlineSync.question(`Question: ${randomNumber} \nYour answer: `);
+
+    if (randomNumber % 2 === 0) {
+      if (yourAnswer === 'yes') {
+        correctAnswers += 1;
+        console.log('Correct!');
+      } else {
+        console.log(`'${yourAnswer}' is wrong answer ;(. Correct answer was 'yes'.\nLet's try again, ${userName}!`);
+      }
+    } else if (randomNumber % 2 !== 0) {
+      if (yourAnswer === 'no') {
+        correctAnswers += 1;
+        console.log('Correct!');
+      } else {
+        console.log(`'${yourAnswer}' is wrong answer ;(. Correct answer was 'no'.\nLet's try again, ${userName}!`);
+      }
+    }
+  }
+
+  if (correctAnswers >= 2) {
+    console.log(`Congratulations, ${userName}!`);
+  } else {
+    console.log('Sorry, you lose :( Try again!');
+  }
+};
+
+isEven();
