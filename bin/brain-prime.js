a112944f0125cb5fe792a4d4ace4e57dd17f9b9b
Daria Timoshevskaya <vileonika@gmail.com>
2022-12-19 23:12:31 +0300

    fixed brain-prime.js

--- bin/brain-prime.js
+++ bin/brain-prime.js
@@ -1,45 +1,7 @@
 #!/usr/bin/env node
 
-import readlineSync from 'readline-sync';
+import { primeGame } from '../src/games/prime-game.js';
 
 console.log('Welcome to the Brain Games!');
-
-const primeGame = () => {
-  const userName = readlineSync.question('May I have your name?: ');
-  console.log(`Hello, ${userName}!`);
-
-  console.log('Answer "yes" if given number is prime. Otherwise answer "no".');
-  let correctAnswers = 0;
-
-  for (let i = 0; i <= 2; i += 1) {
-    const randomNumber = Math.round(Math.random() * (100 - 1) + 1);
-    let result = 'yes';
-
-    if (randomNumber === 1) {
-      result = 'yes';
-    } else if (randomNumber > 1) {
-      for (let n = 2; n < randomNumber; n += 1) {
-        if (randomNumber % n === 0) {
-          result = 'no';
-          break;
-        }
-      }
-    }
-    const yourAnswer = readlineSync.question(`Question: ${randomNumber} \nYour answer: `);
-
-    if (yourAnswer === result) {
-      correctAnswers += 1;
-      console.log('Correct!');
-    } else {
-      console.log(`'${yourAnswer}' is wrong answer ;(. Correct answer was '${result}'.\nLet's try again, ${userName}!`);
-    }
-  }
-
-  if (correctAnswers >= 2) {
-    console.log(`Congratulations, ${userName}!`);
-  } else {
-    console.log('Sorry, you lose :( Try again!');
-  }
-};
 
 primeGame();
