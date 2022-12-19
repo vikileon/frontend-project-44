491926b832c2176bffe4b50380adb815db12cce4
Daria Timoshevskaya <vileonika@gmail.com>
2022-12-19 23:13:26 +0300

    fixed cli.js

--- src/cli.js
+++ src/cli.js
@@ -4,36 +4,3 @@ export function askUserName() {
   const userName = readlineSync.question('May I have your name?: ');
   console.log(`Hello, ${userName}!`);
 }
-
-export function makeProgression() {
-  const firstNumber = Math.round(Math.random() * (10 - 1) + 1);
-  const difference = Math.round(Math.random() * (10 - 1) + 1);
-  const progression = [firstNumber];
-  const progressionLength = Math.round(Math.random() * (10 - 5) + 5);
-  let nextNumber;
-
-  for (let i = 1; i < progressionLength; i += 1) {
-    nextNumber = progression[i - 1] + difference;
-    progression.push(nextNumber);
-  }
-
-  return progression;
-}
-
-export function isPrime() {
-  const randomNumber = Math.round(Math.random() * (100 - 1) + 1);
-  let result = 'yes';
-
-  if (randomNumber === 1) {
-    result = 'yes';
-  } else if (randomNumber > 1) {
-    for (let i = 2; i < randomNumber; i++) {
-      if (randomNumber % i === 0) {
-        result = 'no'
-      } else {
-        result = 'yes'
-      }
-    }
-  }
-  return result;
-}
