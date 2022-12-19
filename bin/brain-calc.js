ce5ef13eee60818e606b9c6ba397ead3fe0a9709
Daria Timoshevskaya <vileonika@gmail.com>
2022-12-19 23:11:04 +0300

    fixed brain-calc.js

--- bin/brain-calc.js
+++ bin/brain-calc.js
@@ -1,52 +1,7 @@
 #!/usr/bin/env node
 
-import readlineSync from 'readline-sync';
+import { calcGame } from '../src/games/calc-game.js';
 
 console.log('Welcome to the Brain Games!');
-
-const calcGame = () => {
-  const userName = readlineSync.question('May I have your name?: ');
-  console.log(`Hello, ${userName}!`);
-
-  console.log('What is the result of the expression?');
-  let correctAnswers = 0;
-  const operators = ['+', '-', '*'];
-  let result;
-  let questionForUser;
-
-  for (let i = 0; i <= 2; i += 1) {
-    const firstNumber = Math.round(Math.random() * (20 - 1) + 1);
-    const secondNumber = Math.round(Math.random() * (20 - 1) + 1);
-    const rand = Math.floor(Math.random() * operators.length);
-    const operator = operators[rand];
-
-    switch (operator) {
-      case '-':
-        result = firstNumber - secondNumber;
-        questionForUser = `${firstNumber} - ${secondNumber}`;
-        break;
-      case '*':
-        result = firstNumber * secondNumber;
-        questionForUser = `${firstNumber} * ${secondNumber}`;
-        break;
-      default:
-        result = firstNumber + secondNumber;
-        questionForUser = `${firstNumber} + ${secondNumber}`;
-    }
-    const yourAnswer = readlineSync.question(`Question: ${questionForUser} \nYour answer: `);
-    if (Number(yourAnswer) === result) {
-      correctAnswers += 1;
-      console.log('Correct!');
-    } else {
-      console.log(`'${yourAnswer}' is wrong answer ;(. Correct answer was '${result}'.\nLet's try again, ${userName}!`);
-    }
-  }
-
-  if (correctAnswers >= 2) {
-    console.log(`Congratulations, ${userName}!`);
-  } else {
-    console.log('Sorry, you lose :( Try again!');
-  }
-};
 
 calcGame();
