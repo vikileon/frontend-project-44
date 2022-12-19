beb254ccf3cf9db3d1cfd14992a812d7400bb094
Daria Timoshevskaya <vileonika@gmail.com>
2022-12-19 23:12:58 +0300

    fixed brain-progression.js

--- bin/brain-progression.js
+++ bin/brain-progression.js
@@ -1,36 +1,7 @@
 #!/usr/bin/env node
 
-import readlineSync from 'readline-sync';
-import { makeProgression } from '../src/cli.js';
+import { progressionGame } from '../src/games/progression-game.js';
 
 console.log('Welcome to the Brain Games!');
-
-const progressionGame = () => {
-  const userName = readlineSync.question('May I have your name?: ');
-  console.log(`Hello, ${userName}!`);
-
-  console.log('What number is missing in the progression??');
-  let correctAnswers = 0;
-  for (let i = 0; i <= 2; i += 1) {
-    const newProgression = makeProgression();
-    const position = Math.round(Math.random() * ((newProgression.length - 1) - 1) + 1);
-    const missingNumber = newProgression[position];
-    newProgression[position] = '..';
-    const yourAnswer = readlineSync.question(`Question: ${newProgression} \nYour answer: `);
-
-    if (Number(yourAnswer) === missingNumber) {
-      correctAnswers += 1;
-      console.log('Correct!');
-    } else {
-      console.log(`'${yourAnswer}' is wrong answer ;(. Correct answer was '${missingNumber}'.\nLet's try again, ${userName}!`);
-    }
-  }
-
-  if (correctAnswers >= 2) {
-    console.log(`Congratulations, ${userName}!`);
-  } else {
-    console.log('Sorry, you lose :( Try again!');
-  }
-};
 
 progressionGame();
