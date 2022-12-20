343e6c81c7ccf0e0feca78bbef37d908e4fdfcf3
Daria Timoshevskaya <vileonika@gmail.com>
2022-12-20 22:21:58 +0300

    fixed progression-game.js

--- src/games/progression-game.js
+++ src/games/progression-game.js
@@ -8,20 +8,23 @@ const progressionGame = () => {
 
   console.log('What number is missing in the progression?');
   let correctAnswers = 0;
+
   for (let i = 0; i <= 2; i += 1) {
     const newProgression = makeProgression();
     const position = getRandomNumber(1, newProgression.length - 1);
     const missingNumber = newProgression[position];
     newProgression[position] = '..';
     const questionForUser = newProgression;
+
+    const correctAnswer = String(missingNumber);
     console.log(`Question: ${questionForUser}`);
     const userAnswer = readlineSync.question('Your answer: ');
 
-    if (Number(userAnswer) === missingNumber) {
+    if (userAnswer === correctAnswer) {
       correctAnswers += 1;
       console.log('Correct!');
     } else {
-      console.log(`'${userAnswer}' is wrong answer ;(. Correct answer was '${missingNumber}'.\nLet's try again, ${userName}!`);
+      console.log(`'${userAnswer}' is wrong answer ;(. Correct answer was '${correctAnswer}'.\nLet's try again, ${userName}!`);
     }
   }
 
