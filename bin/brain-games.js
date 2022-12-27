b9e0fb503fc2695e9df35ab1a71900dea3b40b69
Daria Timoshevskaya <vileonika@gmail.com>
2022-12-27 18:55:52 +0300

    fixed brain-games.js and cli.js

--- bin/brain-games.js
+++ bin/brain-games.js
@@ -1,6 +1,6 @@
 #!/usr/bin/env node
 
-import { askUserName } from '../src/cli.js';
+import askUserName from '../src/cli.js';
 
 console.log('Welcome to the Brain Games!');
 askUserName();
--- src/cli.js
+++ src/cli.js
@@ -1,6 +1,8 @@
 import readlineSync from 'readline-sync';
 
-export function askUserName() {
+const askUserName = () => {
   const userName = readlineSync.question('May I have your name?: ');
   console.log(`Hello, ${userName}!`);
-}
+};
+
+export default askUserName;
