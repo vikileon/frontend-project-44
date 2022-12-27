2f08997dcbb8cfd1cec26f2e93bc6f004d1029d6
Daria Timoshevskaya <vileonika@gmail.com>
2022-12-27 15:56:22 +0300

    fixed brain-progression game

--- bin/brain-progression.js
+++ bin/brain-progression.js
@@ -2,6 +2,4 @@
 
 import progressionGame from '../src/games/progression-game.js';
 
-console.log('Welcome to the Brain Games!');
-
 progressionGame();
--- src/games/progression-game.js
+++ src/games/progression-game.js
@@ -1,38 +1,23 @@
-import readlineSync from 'readline-sync';
 import getRandomNumber from '../getRandomNumber.js';
 import makeProgression from '../makeProgression.js';
+import gameLogic from '../index.js';
+
+const gameDescription = 'What number is missing in the progression?';
+
+const progressionData = () => {
+  const newProgression = makeProgression();
+  const position = getRandomNumber(1, newProgression.length - 1);
+  const missingNumber = newProgression[position];
+  newProgression[position] = '..';
+  const questionForUser = newProgression;
+
+  const correctAnswer = String(missingNumber);
+
+  return [questionForUser, correctAnswer];
+};
 
 const progressionGame = () => {
-  const userName = readlineSync.question('May I have your name?: ');
-  console.log(`Hello, ${userName}!`);
-
-  console.log('What number is missing in the progression?');
-  let correctAnswers = 0;
-
-  for (let i = 0; i <= 2; i += 1) {
-    const newProgression = makeProgression();
-    const position = getRandomNumber(1, newProgression.length - 1);
-    const missingNumber = newProgression[position];
-    newProgression[position] = '..';
-    const questionForUser = newProgression;
-
-    const correctAnswer = String(missingNumber);
-    console.log(`Question: ${questionForUser}`);
-    const userAnswer = readlineSync.question('Your answer: ');
-
-    if (userAnswer === correctAnswer) {
-      correctAnswers += 1;
-      console.log('Correct!');
-    } else {
-      console.log(`'${userAnswer}' is wrong answer ;(. Correct answer was '${correctAnswer}'.\nLet's try again, ${userName}!`);
-    }
-  }
-
-  if (correctAnswers >= 2) {
-    console.log(`Congratulations, ${userName}!`);
-  } else {
-    console.log('Sorry, you lose :(');
-  }
+  gameLogic(gameDescription, progressionData);
 };
 
 export default progressionGame;
