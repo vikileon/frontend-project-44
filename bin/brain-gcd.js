a594ce28414211c1315eedbfb51228fd2aa47d23
Daria Timoshevskaya <vileonika@gmail.com>
2022-12-27 15:55:14 +0300

    fixed brain-gcd game

--- bin/brain-gcd.js
+++ bin/brain-gcd.js
@@ -2,6 +2,4 @@
 
 import gcdGame from '../src/games/gcd-game.js';
 
-console.log('Welcome to the Brain Games!');
-
 gcdGame();
--- src/games/gcd-game.js
+++ src/games/gcd-game.js
@@ -1,56 +1,40 @@
-import readlineSync from 'readline-sync';
 import getRandomNumber from '../getRandomNumber.js';
+import gameLogic from '../index.js';
+
+const gameDescription = 'Find the greatest common divisor of given numbers.';
+
+const gcdData = () => {
+  const firstNumber = getRandomNumber(1, 100);
+  const secondNumber = getRandomNumber(1, 100);
+
+  const questionForUser = `${firstNumber} ${secondNumber}`;
+
+  let gcd;
+  let a = firstNumber;
+  let b = secondNumber;
+
+  if (a === b) {
+    gcd = a;
+  } else {
+    while (a !== b) {
+      if (a > b) {
+        a -= b;
+      } else {
+        const k = a;
+        a = b;
+        b = k;
+        a -= b;
+      }
+      gcd = a;
+    }
+  }
+  const correctAnswer = String(gcd);
+
+  return [questionForUser, correctAnswer];
+};
 
 const gcdGame = () => {
-  const userName = readlineSync.question('May I have your name?: ');
-  console.log(`Hello, ${userName}!`);
-
-  console.log('Find the greatest common divisor of given numbers.');
-  let correctAnswers = 0;
-  let questionForUser;
-
-  for (let i = 0; i <= 2; i += 1) {
-    const firstNumber = getRandomNumber(1, 100);
-    const secondNumber = getRandomNumber(1, 100);
-
-    questionForUser = `Question: ${firstNumber} ${secondNumber}`;
-    let nod;
-    let a = firstNumber;
-    let b = secondNumber;
-
-    if (a === b) {
-      nod = a;
-    } else {
-      while (a !== b) {
-        if (a > b) {
-          a -= b;
-        } else {
-          const k = a;
-          a = b;
-          b = k;
-          a -= b;
-        }
-        nod = a;
-      }
-    }
-
-    console.log(`Question: ${questionForUser}`);
-    const userAnswer = readlineSync.question('Your answer: ');
-    const correctAnswer = String(nod);
-
-    if (userAnswer === correctAnswer) {
-      correctAnswers += 1;
-      console.log('Correct!');
-    } else {
-      console.log(`'${userAnswer}' is wrong answer ;(. Correct answer was '${correctAnswer}'.\nLet's try again, ${userName}!`);
-    }
-  }
-
-  if (correctAnswers >= 2) {
-    console.log(`Congratulations, ${userName}!`);
-  } else {
-    console.log('Sorry, you lose :(');
-  }
+  gameLogic(gameDescription, gcdData);
 };
 
 export default gcdGame;
